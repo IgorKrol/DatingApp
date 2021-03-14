4c049496a85ffff28ff3af06008c9d716f3ab1a8
IgorKrol <igork08@gmail.com>
2021-03-14 09:59:23 +0200

    Part9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -15,7 +15,6 @@ export class AppComponent implements OnInit{
   constructor(private http: HttpClient, private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -24,11 +23,4 @@ export class AppComponent implements OnInit{
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error =>{
-      console.log(error);
-    })
-  }
 }
