c27e08853fb130318f4fc741561dd1ce49ffd3e1
IgorKrol <igork08@gmail.com>
2021-03-17 19:59:39 +0200

    Part11

--- client/src/app/_models/shared.module.ts
+++ client/src/app/_models/shared.module.ts
@@ -5,6 +5,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [],
@@ -16,7 +17,8 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     }),
     TabsModule.forRoot(),
     NgxGalleryModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    FileUploadModule
   ],
 
   exports: [
@@ -24,6 +26,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     ToastrModule,
     TabsModule,
     NgxGalleryModule,
+    FileUploadModule,
     NgxSpinnerModule
   ]
 })
